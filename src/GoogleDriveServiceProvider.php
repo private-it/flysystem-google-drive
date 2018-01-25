8c3f968d21634bc59d3d84463b941e631a6ba4cd
Rendol <sapegin@private-it.ru>
2018-01-25 15:24:45 +0700

    set GoogleSheetsPathManager as default PathManager

--- src/GoogleDriveServiceProvider.php
+++ src/GoogleDriveServiceProvider.php
@@ -15,26 +15,36 @@ class GoogleDriveServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->extend(Google_Client::class, function ($client) {
+
+        app()->extend(Google_Client::class, function ($client)
+        {
             $config = config('filesystems.disks.googleDrive');
             /** @var Google_Client $client */
             $client->setClientId($config['clientId']);
             $client->setClientSecret($config['clientSecret']);
             $client->refreshToken($config['refreshToken']);
+
             return $client;
         });
 
-        app()->bind(GoogleSheetsPathManager::class, function () {
+        app()->bind(GoogleSheetsPathManager::class, function ()
+        {
             $config = config('filesystems.disks.googleDrive');
             /** @var Google_Client $client */
-            $client = app()->make(Google_Client::class);
+            $client       = app()->make(Google_Client::class);
             $sheetService = new \Google_Service_Sheets($client);
-            $pathManager = new GoogleSheetsPathManager($sheetService, $config['sheetId'], Storage::disk('local'));
+            $pathManager  = new GoogleSheetsPathManager($sheetService, $config['sheetId'], Storage::disk('local'));
             $pathManager->updateCacheFile();
+
             return $pathManager;
         });
 
-        Storage::extend('googleDrive', function ($app, $config) {
+        if ( ! app()->bound(PathManager::class)) {
+            app()->bind(PathManager::class, GoogleSheetsPathManager::class);
+        }
+
+        Storage::extend('googleDrive', function ($app, $config)
+        {
             /** @var Google_Client $client */
             $client = app()->make(Google_Client::class);
             /** @var PathManager $client */
@@ -58,5 +68,3 @@ class GoogleDriveServiceProvider extends ServiceProvider
         //
     }
 }
-
-
